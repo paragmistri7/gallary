fc47dbf428a6bdcdc10c9f761588f610c1d623f6
paragmistri7 <paragmistri7@gmail.com>
2026-04-27 12:46:35 +0530

    zzz

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,13 @@
 "use client";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import { BrowserRouter } from "react-router-dom";
 import AuthPage from "./login.tsx/page";
 const App = () => {
   return (
     <>
-      <BrowserRouter>
         <Provider store={store}>
           <AuthPage/>
         </Provider>
-      </BrowserRouter>
     </>
   );
 };
