431d565633feeb4beb6fa5e0d1f5067ed425aa77
paragmistri7 <paragmistri7@gmail.com>
2026-04-27 13:06:58 +0530

    ccvv

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-    images: {
+  images: {
+    unoptimized: true,
     domains: ['cdn.dummyjson.com'],
       
   },
